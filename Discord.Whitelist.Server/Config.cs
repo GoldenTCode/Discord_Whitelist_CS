5e15d71cd19555f180236fc4e1bc66e8b9fdffeb
Xd_Golden_Tiger <68713969+XdGoldenTigerOfficial@users.noreply.github.com>
2022-06-15 20:33:35 +1200

    Add files via upload

---
+++ Discord.Whitelist.Server/Config.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using SharpConfig;
+
+namespace Discord.Whitelist.Server
+{
+    public class Config : BaseScript
+    {
+        public static WhitelistConfig WhitelistConfig;
+    }
+
+    public struct WhitelistConfig
+    {
+        public string GUILDID;
+        public string BOTTOKEN;
+        public string ROLEID;
+        public string DISCORDINV;
+
+        public static void Load()
+        {
+            WhitelistConfig config = new WhitelistConfig()
+            {
+                GUILDID = "",
+                BOTTOKEN = "",
+                ROLEID = "",
+                DISCORDINV = ""
+            };
+
+            var configFile = Configuration.LoadFromFile(string.Format("{0}/WhitelistCFG.ini", GetResourcePath(GetCurrentResourceName())));
+
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(configFile.ToString()))
+                {
+                    var whitelistConfigSection = configFile["WHITELIST"];
+                    config.GUILDID = whitelistConfigSection["GUILDID"].StringValue;
+                    config.BOTTOKEN = whitelistConfigSection["BOTTOKEN"].StringValue;
+                    config.ROLEID = whitelistConfigSection["ROLEID"].StringValue;
+
+                    Config.WhitelistConfig = config;
+                    Debug.WriteLine("[Discord WL] SUCCESS - The Config has been setup succesfully!");
+                }
+                else
+                {
+                    Debug.WriteLine("[Discord WL] ERROR - The Config file is empty, Please make sure the Config is setup correctly!");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[Discord WL] ERROR - Something went wrong while loading the Config file.\n{ex}");
+            }
+        }
+    }
+}
---
+++ Discord.Whitelist.Server/Main.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+using CitizenFX.Core;
+using Newtonsoft.Json;
+using static CitizenFX.Core.Native.API;
+
+namespace Discord.Whitelist.Server
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            Debug.WriteLine("Discord Whitelist Starting!");
+        }
+
+        [EventHandler("onServerResourceStart")]
+        private void OnServerResourceStart(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName) return;
+
+            WhitelistConfig.Load();
+        }
+
+        [EventHandler("playerConnecting")]
+        private async void OnPlayerConnecting([FromSource] Player player, string playerName, dynamic setKickReason, dynamic deferrals)
+        {
+            try
+            {
+                await Delay(0);
+                deferrals.defer();
+
+                string DiscordID = player.Identifiers["discord"];
+
+                deferrals.update($"Checking for Discord ID...");
+
+                if (string.IsNullOrEmpty(DiscordID)) deferrals.done($"You must have Discord open (and Linked) in order to connect to the server!");
+
+                // ---------------- Allows us to access the Discord API - Without this HTTP Client Handler the API will error. ---------------- //
+                HttpClientHandler httpClientHandler = new HttpClientHandler
+                {
+                    ClientCertificateOptions = ClientCertificateOption.Automatic,
+                    UseProxy = true,
+                    UseDefaultCredentials = true
+                };
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+                // ---------------------------------------------------------------------------------------------------------------------------- //
+
+                using (var DiscordAPI = new HttpClient(httpClientHandler))
+                {
+                    DiscordAPI.DefaultRequestHeaders.Accept.Clear();
+                    DiscordAPI.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    DiscordAPI.DefaultRequestHeaders.Add("Authorization", $"Bot {Config.WhitelistConfig.BOTTOKEN}");
+
+                    HttpResponseMessage memberObjRes = DiscordAPI.GetAsync($"https://discord.com/api/v9/guilds/{Config.WhitelistConfig.GUILDID}/members/{DiscordID}").GetAwaiter().GetResult();
+
+                    if (!memberObjRes.IsSuccessStatusCode)
+                    {
+                        if (memberObjRes.StatusCode == HttpStatusCode.NotFound)
+                        {
+                            Debug.WriteLine($"------------------------------------------------------------------------------------------------------\n{playerName} ({DiscordID}) was unable to connect to the server.\nReason: Not in the Discord Server.\n------------------------------------------------------------------------------------------------------");
+                            deferrals.presentCard("{\"type\":\"AdaptiveCard\",\"version\":\"1.5\",\"actions\":[{\"type\":\"Action.OpenUrl\",\"title\":\"Join The Discord Server\",\"style\":\"positive\",\"url\":\"" + Config.WhitelistConfig.DISCORDINV + "\"}],\"body\":[{\"type\":\"TextBlock\",\"text\":\"You must be in our discord server (and whitelisted) in order to connect to our server(s)!\",\"wrap\":true}]}");
+                            return;
+                        }
+                        else
+                        {
+                            deferrals.done($"Something went wrong while connecting to the server!");
+                            var memberErrObjStr = await memberObjRes.Content.ReadAsStringAsync();
+                            Debug.WriteLine($"Error {memberObjRes.StatusCode}: {memberErrObjStr}");
+                        }
+                    }
+
+                    var memberObjStr = await memberObjRes.Content.ReadAsStringAsync();
+
+                    GuildMember memberObj = JsonConvert.DeserializeObject<GuildMember>(memberObjStr);
+
+                    if (!memberObj.Roles.Contains(Config.WhitelistConfig.ROLEID))
+                    {
+                        deferrals.done($"You must be Whitelisted in order to connect to the server!");
+                        Debug.WriteLine($"------------------------------------------------------------------------------------------------------\n{playerName} ({DiscordID}) was unable to connect to the server.\nReason: Not Whitelisted in the Discord Server.\n------------------------------------------------------------------------------------------------------");
+                        return;
+                    }
+
+                    Debug.WriteLine($"------------------------------------------------------------------------------------------------------\n{playerName} ({DiscordID}) connected successfully to the server!\n------------------------------------------------------------------------------------------------------");
+
+                    deferrals.done();
+                }
+            }
+            catch (Exception ex)
+            {
+                deferrals.done($"Something went wrong while connecting to the server!");
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    #region Just a whole lot of class shit for Discord (Line 82 to be exact)
+    public class User
+    {
+        [JsonProperty("id")]
+        public ulong Id { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("avatar")]
+        public string Avatar { get; set; }
+
+        [JsonProperty("avatar_decoration")]
+        public string AvatarDecoration { get; set; }
+
+        [JsonProperty("discriminator")]
+        public string Discriminator { get; set; }
+
+        [JsonProperty("public_flags")]
+        public int PublicFlags { get; set; }
+    }
+    public class GuildMember
+    {
+        [JsonProperty("avatar")]
+        public string Avatar { get; set; }
+
+        [JsonProperty("communication_disabled_until")]
+        public DateTimeOffset? TimedOutUntil { get; set; }
+
+        [JsonProperty("flags")]
+        public int Flags { get; set; }
+
+        [JsonProperty("is_pending")]
+        public bool IsPending { get; set; }
+
+        [JsonProperty("joined_at")]
+        public DateTimeOffset JoinedAt { get; set; }
+
+        [JsonProperty("nick")]
+        public string Nick { get; set; }
+
+        [JsonProperty("pending")]
+        public bool Pending { get; set; }
+
+        [JsonProperty("premium_since")]
+        public DateTimeOffset? PremiumSince { get; set; }
+
+        [JsonProperty("roles")]
+        public List<string> Roles { get; set; }
+
+        [JsonProperty("user")]
+        public User User { get; set; }
+
+        [JsonProperty("mute")]
+        public bool Mute { get; set; }
+
+        [JsonProperty("deaf")]
+        public bool Deaf { get; set; }
+    }
+    #endregion
+}
